6c50d4eee39fef5e75e8e0a4bfec13a41677cda7
jcbevc <jcbevc@gmail.com>
2023-04-21 16:14:21 +0200

    added crafting

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,20 +1,28 @@
-// priority: 0
-
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
-
-console.info('Hello, World! (You will see this line every time server resources reload)')
-
 onEvent('recipes', event => {
-	// Change recipes here
-})
+	event.shaped(
+		Item.of('minecraft:stone', 3), // arg 1: output
+		[ 
+		  'A B', 
+		  ' C ', // arg 2: the shape (array of strings)
+		  'B A'  
+		],
+		{
+		  A: 'minecraft:andesite', 
+		  B: 'minecraft:diorite',  //arg 3: the mapping object
+		  C: 'minecraft:granite'   
+		}
+	  )
 
-onEvent('item.tags', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+	  event.remove({id: 'xreliquary:uncrafting/stick'})
 
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+	  event.shapeless(
+		Item.of('minecraft:dandelion', 3), // arg 1: output
+		[ 
+		  'minecraft:bone_meal',
+		  'minecraft:yellow_dye', 	//arg 2: the array of inputs
+		  '3x minecraft:ender_pearl'
+		]
+	  )
+
+	  event.smelting('minecraft:lava_bucket', 'minecraft:obsidian')
 })
